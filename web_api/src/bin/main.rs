85f0a2a286d7af27e59a71b69c6a5eefed1732d2
Karatla <3164926@qq.com>
2021-01-21 18:25:16 +0800

    bug fix

--- web_api/src/bin/main.rs
+++ web_api/src/bin/main.rs
@@ -7,9 +7,9 @@ async fn main() -> std::io::Result<()> {
 
     //std::env::set_var("RUST_LOG", "actix_web=info");
 
-    //link is "https://192.168.0.139:443";
-    let bind = "127.0.0.1:8080";
-    //let bind = "0.0.0.0:443";
+    //find ip by : ifconfig 
+    //let bind = "127.0.0.1:8080";
+    let bind = "0.0.0.0:443";
 
     // load ssl key
     let config = ssl_config::ssl_load();
@@ -39,8 +39,8 @@ async fn main() -> std::io::Result<()> {
             .service(web_rount::question_images_get)
             .service(web_rount::academy_images_get)
     })
-    .bind(&bind)?
-    //.bind_rustls(&bind, config)?
+    //.bind(&bind)?
+    .bind_rustls(&bind, config)?
     .run()
     .await
 }
